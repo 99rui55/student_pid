38f472840a36dc3340762521f4e3681984d7262a
tomer-3431 <tomer10.katz@gmail.com>
2023-08-16 18:11:58 +0300

    changes few things

--- src/main/java/frc/robot/commands/VelocityPIDCommand.java
+++ src/main/java/frc/robot/commands/VelocityPIDCommand.java
@@ -3,7 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Chassis;
-import static frc.robot.Constants.ChassisConstants.*;
+import static frc.robot.Constants.ChassisConstants;
 
 
 public class VelocityPIDCommand extends CommandBase {
@@ -16,7 +16,7 @@ public class VelocityPIDCommand extends CommandBase {
 
     @Override
     public void initialize() {
-        double velocity = SmartDashboard.getNumber(AutoVelocityID, 0);
+        double velocity = SmartDashboard.getNumber(ChassisConstants.AutoVelocityID, 0);
         chassis.setPID();
         chassis.setVelocity(velocity);
     }
--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -6,30 +6,27 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import static frc.robot.Constants.ChassisConstants.*;
-import frc.robot.RobotContainer;
+import static frc.robot.Constants.ChassisConstants;
 
 public class Chassis extends SubsystemBase {
 
     TalonFX left;  // shortcut for leftMotors[0]
     TalonFX right;
-    RobotContainer container; // for future use
 
-    public Chassis(RobotContainer container) {
-        this.container = container;
-        left = initMotors(LeftFrontMotor, LeftBackMotor, LeftInverted);
-        right = initMotors(RightFrontMotor, RightBackMotor, RightInverted);
+    public Chassis() {
+        left = initMotors(ChassisConstants.LeftFrontMotor, ChassisConstants.LeftBackMotor, ChassisConstants.LeftInverted);
+        right = initMotors(ChassisConstants.RightFrontMotor, ChassisConstants.RightBackMotor, ChassisConstants.RightInverted);
     }
 
     // Init motors for one side
-    private TalonFX initMotors(int main, int follower, boolean invert) {
-        TalonFX mainn = new TalonFX(main);
-        TalonFX followerr = new TalonFX(follower);
-        mainn.setInverted(invert);
-        followerr.setInverted(invert);
-        followerr.follow(mainn);
-        setPID(mainn, VelocityKP, VelocityKI, VelocityKD);
-        return mainn;
+    private TalonFX initMotors(int mainn, int followerr, boolean invert) {
+        TalonFX main = new TalonFX(mainn);
+        TalonFX follower = new TalonFX(followerr);
+        main.setInverted(invert);
+        follower.setInverted(invert);
+        follower.follow(main);
+        setPID(main, ChassisConstants.VelocityKP, ChassisConstants.VelocityKI, ChassisConstants.VelocityKD);
+        return main;
     }
 
     public void setPower(double leftt, double rightt) {
@@ -64,17 +61,17 @@ public class Chassis extends SubsystemBase {
     }
 
     public void setPID() { 
-        setPID(SmartDashboard.getNumber("Velocity KP", VelocityKP),
-                SmartDashboard.getNumber("Velocity KI", VelocityKI),
-                SmartDashboard.getNumber("Velocity KD", VelocityKD));
+        setPID(SmartDashboard.getNumber("Velocity KP", ChassisConstants.VelocityKP),
+                SmartDashboard.getNumber("Velocity KI", ChassisConstants.VelocityKI),
+                SmartDashboard.getNumber("Velocity KD", ChassisConstants.VelocityKD));
     }
 
     // get functions
     public double getLeftDistance() {
-        return left.getSelectedSensorPosition()/PulsePerMeter;
+        return left.getSelectedSensorPosition()/ChassisConstants.PulsePerMeter;
     }
     public double getRightDistance() {
-        return right.getSelectedSensorPosition()/PulsePerMeter;
+        return right.getSelectedSensorPosition()/ChassisConstants.PulsePerMeter;
     }
     public double getDistance() {
         return (getLeftDistance() + getRightDistance())/2;
@@ -98,20 +95,20 @@ public class Chassis extends SubsystemBase {
         builder.addDoubleProperty("Left Velocity", this::getLeftVelocity, null);
         builder.addDoubleProperty("Right Velocity", this::getRightVelocity, null);
         builder.addDoubleProperty("Velocity", this::getLeftVelocity, null);
-        SmartDashboard.putNumber("Velocity KP", VelocityKP);
-        SmartDashboard.putNumber("Velocity KD", VelocityKD);
-        SmartDashboard.putNumber("Velocity KI", VelocityKI);
-        addNTField(AutoVelocityID, 1);
+        SmartDashboard.putNumber("Velocity KP", ChassisConstants.VelocityKP);
+        SmartDashboard.putNumber("Velocity KD", ChassisConstants.VelocityKD);
+        SmartDashboard.putNumber("Velocity KI", ChassisConstants.VelocityKI);
+        addNTField(ChassisConstants.AutoVelocityID, 1);
     }
 
 
     // utilities
     public static double TalonVelocityToVelocity(double velocity) {
-        return velocity * 10 / PulsePerMeter;
+        return velocity * 10 / ChassisConstants.PulsePerMeter;
     }
     
     public static double VelocityToTalonVelocity(double velocity) {
-        return velocity * PulsePerMeter / 10;
+        return velocity * ChassisConstants.PulsePerMeter / 10;
     }
 
     // add network table field
