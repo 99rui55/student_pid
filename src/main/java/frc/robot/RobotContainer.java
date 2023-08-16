939c238661ad3cd28d2396c13df6a707fd75c5c8
tomer-3431 <tomer10.katz@gmail.com>
2023-08-16 18:12:10 +0300

    changes few things

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    chassis = new Chassis(this);
+    chassis = new Chassis();
     configurBindings();
    
   }
