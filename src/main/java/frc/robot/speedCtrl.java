a17401b9af07cb547cd4a372b65becb4ca664863
shayFarj <shayfarjoon0@gmail.com>
2023-09-06 17:17:18 +0300

    Saving before I change completly speedctrl

--- src/main/java/frc/robot/speedCtrl.java
+++ src/main/java/frc/robot/speedCtrl.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 
+import java.lang.management.OperatingSystemMXBean;
+
 import frc.robot.Constants.OperatorConstants;
 
 public class speedCtrl {
@@ -8,6 +10,11 @@ public class speedCtrl {
     double maxVel;
     double maxAcc;
 
+    /**
+     * 
+     * @param maxVel - Maximum Velocity (Meters per second)
+     * @param maxAcc - Maximum Acceleration (Meters per second)
+     */
     public speedCtrl(double maxVel, double maxAcc)
     {
         this.maxAcc = maxAcc;
@@ -16,72 +23,56 @@ public class speedCtrl {
 
     /**
      * 
-     * @param rD - Remaining Distance
-     * @param cVel - Current Velocity
-     * @param tgtVel - Target Velocity
-     * @param endVel - End Velocity
-     * @return Returns the velocity needed in the current cycle in order to mantain a Trapzoid Motion Profile.
+     * @param rD - Remaining Distance (Meters)
+     * @param cVel - Current Velocity (Meters per Second)
+     * @param tgtVel - Target Velocity (Meters per second)
+     * @param endVel - End Velocity (Meters per second)
+     * @return Returns the velocity (Meters per cycle time) needed in the current cycle in order to mantain a Trapzoid Motion Profile.
      * According to the remaining distance, given the current velocity, target velocity, and end velocity.
      */
     public double calculate(double rD /**Remaining Distance*/,double cVel, double tgtVel, double endVel)
     {
+        //Acceleration and the next velocity are in Meters per cycle time, not seconds.
+        double acc = Math.signum(tgtVel - cVel) * this.maxAcc * OperatorConstants.cTime;
+        double nextV = cVel * OperatorConstants.cTime + acc;
 
-        //Acceleration according to cycle time and direction (backward, forward)
-        double acc = Math.signum(tgtVel - cVel) * maxAcc * OperatorConstants.cTime;
-
-
-        //Make sure that the target vel does not exceed the max vel
-        if(tgtVel > this.maxVel)
-            tgtVel = this.maxVel;
-
-        //Next vel in next cycle = current vel plus acc
-        double nextV = cVel + acc;
-
-        /*Check if the remaining distance in the next cycle (with the current vel plus acc)
-         will be enough for the robot to deaccelerate or accelerate to the requested vel at the end   */
-        if(rD -  accDC(cVel,endVel) > accD(nextV, endVel))
-            //Check if the velocity in the next cycle doesn't exceed the target vel
-            if(Math.abs(tgtVel - cVel) > this.maxAcc)
-                //If the vel is less than the target vel, return the vel added with acc
-                return nextV;
-            else
-                return tgtVel;
-        else
+        if(rD - accDC(nextV / OperatorConstants.cTime, tgtVel) > accD(nextV / OperatorConstants.cTime, tgtVel))
         {
-            //Since there will be not enough distance to deaccelerate or accelerate to the end vel in the next cycle
-            //(if we decided to keep the current vel).
-            //Then, the robot should deaccelerate or accelerate beforehand.
-
-
-            acc = Math.signum(endVel - cVel) * maxAcc * OperatorConstants.cTime;
-            nextV = cVel + acc;
-
-            
-            if(Math.abs(tgtVel - cVel) > this.maxAcc)
-                //If the vel in the next cycle (current vel with deacceleration or acceleration) does not exceed the end velocity,
-                //Let it continue accelerating or deacelerating
+            if(Math.abs(tgtVel - nextV / OperatorConstants.cTime) > this.maxAcc){
+                System.out.println("Acc to Target Speed");
                 return nextV;
-            else
-                return endVel;
+            }
+            else{
+                System.out.println("Keep Target Speed");
+                return tgtVel * OperatorConstants.cTime;
+            }
+        }
+        else{
+            acc = Math.signum(endVel - cVel) * this.maxAcc * OperatorConstants.cTime;
+            nextV = cVel * OperatorConstants.cTime + acc;
+            if(Math.abs(endVel - nextV / OperatorConstants.cTime) > this.maxAcc)
+            {
+                System.out.println("Acc to End Speed");
+                return nextV;
+            }
+            else{
+                System.out.println("Keep End Speed");
+                return endVel * OperatorConstants.cTime;
+            }
         }
     }
 
    /**
     * The distance required to accelerate from current velocity to a target velocity,
     * given the maximum acceleration.
-    * @param cVel - Current Velocity
-    * @param tgtVel - Target Velocity
+    * @param cVel - Current Velocity (Meters per seccond)
+    * @param tgtVel - Target Velocity (Meters per second)
     * @return The distance required to accelerate from current velocity to a target velocity,
     * given the maximum acceleration.
     */
     double accD(double cVel,double tgtVel)
     {
-        double acc;
-        //Checking the direction of the acceleration (backward,forward)
-        if(cVel > tgtVel)
-            acc = -maxAcc;
-        else
-            acc = maxAcc;
+        double acc = Math.signum(tgtVel - cVel) * maxAcc;
         double time = (tgtVel - cVel)/acc;
         
         return time * cVel + 0.5 * acc * Math.pow(time, 2);
@@ -89,7 +80,8 @@ public class speedCtrl {
 
     /**
      * 
-     * @param cVel Current Velocity
+     * @param cVel Current Velocity (Meters per second)
+     * @param tgtVel Target Velocity (Meters per second)
      * @return The distance passed after one cycle, given the current velocity and the maximum acceleration.
      */
     double accDC(double cVel,double tgtVel)
