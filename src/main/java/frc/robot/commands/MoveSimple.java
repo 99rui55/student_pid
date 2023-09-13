b2bb9f63b195debd0ab7ce0080752c2b03b90f28
YonatanSurujon <yo7na7@gmail.com>
2023-09-13 18:27:36 +0300

    movesimple

---
+++ src/main/java/frc/robot/commands/MoveSimple.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Chassis;
+import static frc.robot.Constants.ChassisConstants.*;
+
+
+public class MoveSimple extends CommandBase {
+    Chassis chassis;
+    private double velocity = 0.5;
+
+    public MoveSimple(Chassis chassis) {
+        this.chassis = chassis;
+        addRequirements(chassis);
+    }
+
+    @Override
+    public void initialize() {
+        chassis.setVelocity(velocity);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        chassis.stop();
+    }
+}
